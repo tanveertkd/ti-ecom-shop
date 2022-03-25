3878369968ec025cd2bf1589defac725cd33c237
tanveertkd <hazarikatanveer12@gmail.com>
2022-03-25 22:11:29 +0530

    feat: responsive sidebar

--- src/components/Showcase/Showcase.jsx
+++ src/components/Showcase/Showcase.jsx
@@ -16,30 +16,30 @@ const Showcase = () => {
 
     return(
         <div className="showcase-body">
-            <h2>Categories</h2>
+            <h2 className="showcase-heading">Categories</h2>
             <div className="card-parent">
             {categoryItems?.map(item => {
                 return(
                     <div className="card" key={item.id}>
                         <div className="card-container card-img-vertical">
-                        <img
-                            src={item.asset}
-                            alt="card asset"
-                            className="card-image"
-                            />
-                        <div className="card-body-content">
-                        <h3 className="card-head">{item.categoryName}</h3>
-                        <p className="card-body">
-                            {item.description}
-                        </p>
-                        <p className="card-body">
-                            Staring from Rs 999!
-                        </p>
-                        <div className="card-buttons">
-                            <button className="card-btn-buy">View Products</button>
+                            <img
+                                src={item.asset}
+                                alt="card asset"
+                                className="card-image"
+                                />
+                            <div className="card-body-content">
+                                <h3 className="card-head">{item.categoryName}</h3>
+                                <p className="card-body">
+                                    {item.description}
+                                </p>
+                                <p className="card-body">
+                                    Staring from Rs 999!
+                                </p>
+                                <div className="card-buttons">
+                                    <button className="card-btn-buy">View Products</button>
+                                </div>
+                            </div>
                         </div>
-                        </div>
-                    </div>
                     <span className="card-badge">NEW!</span>
                 </div>
                 )
